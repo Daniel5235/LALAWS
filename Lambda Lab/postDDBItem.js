8304471c60a796698fa3fb1716124578efdca056
Daniel Richardson <>
2019-03-04 08:11:44 -0600

    Injected intentional problems for the hands on lab.

--- Lambda Lab/postDDBItem.js
+++ Lambda Lab/postDDBItem.js
@@ -4,16 +4,19 @@ var AWS = require('aws-sdk'),
 	uuid = require('uuid'),
 	documentClient = new AWS.DynamoDB.DocumentClient(); // used to initiate the DOCClient API for AWS
  
-exports.additem = function(event, context, callback){
+additem = function(event, context, callback){
 	var params = {
 		Item : {
 			"ID" : uuid.v1(),
 			"userid" : event.userid, //these values are being passed from api gateway via the event object
-			"eventkey" : event.keyvalue,
-			"eventkey2" : event.keyvalue,
-			"eventkey3" : event.keyvalue,
-			"eventkey4" : event.keyvalue,
-			"eventkey5" : event.keyvalue
+			//todo: add an event key to include a data value.
+			
+			
+
+			
+			//"eventkey" : event.keyvalue,
+		
+		
 		},
 		TableName : process.env.TABLE_NAME // veriable set in lambda console 
 
